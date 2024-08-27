9b98a12e369676515d8d459255122606d8b0ef16
ChristopherCarlton <ccarlton2001@gmail.com>
2024-08-26 18:42:04 -0700

    made the Investors & Partners video responsive on all screens

--- src/app/investors-&-partners/page.tsx
+++ src/app/investors-&-partners/page.tsx
@@ -2,23 +2,22 @@ import React from 'react';
 
 const Video: React.FC = () => {
   return (
-    <div className="relative w-full h-screen">
-      <video
-        src="/images/LivAI-V.mp4"
-        autoPlay
-        loop
-        muted
-        playsInline
-        className="absolute inset-0 w-full h-full object-cover"
-      ></video>
+    <div className="relative w-full bg-[#080c3c] pt-8 pb-[30rem] md:pb-[35rem] lg:pb-[40rem] xl:pb-[65rem]">
+      <div className="w-full h-auto">
+        <video
+          src="/images/LivAI-V.mp4"
+          autoPlay
+          loop
+          muted
+          playsInline
+          className="absolute inset-0 w-full h-auto object-contain"
+        ></video>
+      </div>
     </div>
   );
 };
 
 export default Video;
-
-
-
 
 // import React from 'react';
 
